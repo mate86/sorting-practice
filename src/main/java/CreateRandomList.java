76c46cce7b394dd933733f09be3b0d831f07d5b7
Balázs Máté <balazsm86@gmail.com>
2017-07-14 15:18:41 +0200

    Creating new random sorted list

--- src/main/java/CreateRandomList.java
+++ src/main/java/CreateRandomList.java
@@ -6,11 +6,20 @@ import java.util.Random;
  * Created by mate on 2017.07.14..
  */
 public class CreateRandomList {
+
     public static List unsorted(int n) {
-        List<Integer> sortedList = new ArrayList<>();
+        List<Integer> newList = new ArrayList<>();
         for (int i = 0; i < n; i++) {
-            sortedList.add(new Random().nextInt(99) + 1);
+            newList.add(new Random().nextInt(99) + 1);
         }
-        return sortedList;
+        return newList;
+    }
+
+    public static List sorted(int n) {
+        List<Integer> newList = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            newList.add(i + 1);
+        }
+        return newList;
     }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,7 +4,9 @@ import java.util.List;
  * Created by mate on 2017.07.14..
  */
 public class Main {
+
     public static void main(String[] args) {
         List<Integer> unsortedList = CreateRandomList.unsorted(10);
+        List<Integer> sortedList = CreateRandomList.sorted(10);
     }
 }
